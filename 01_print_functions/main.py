a6840c6db57a671ea3d8bbd0871e35070be55322
Hufsa Ghazal <ufsagh123@gmail.com>
2026-04-28 01:36:07 +0500

    first commit - print functions practice

---
+++ 01_print_functions/main.py
@@ -0,0 +1,33 @@
+# Print Functions
+
+# Built-in Python function used to display output to users
+# Use cases: communicate, show results, debug, test
+
+# \" allows double quotes inside a string
+
+print("Hi \"Python\"")
+print('Hi "Python"') # also valid
+print('Hi \'Python\'') # escaped single quotes
+
+# \\ prints a backslash character
+print("Path: C:\\users\\Hufsa")
+
+# \n new line (breaks text into multiple lines)
+print("Welcome to Python\nLet's start learning")
+print("Learning Python\n") # trailing newline
+print("Step 1: Basics\n\n\nStep 2: Practice") # two blank lines
+
+#\t tab (adds horizontal spacing)
+print("Topic:\tPython Basics")
+
+#\b backspace (experimental behavior)
+print("Helloo\b!")
+
+# Example
+print("Grocery List:\n\t- Bread\n\t- Eggs\n\t- Fruits")
+
+# Triple quotes (multi-line string)
+print("""Grocery List:
+\t- Bread
+\t- Eggs
+\t- Fruits""")
